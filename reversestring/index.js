40fb8560dea80dec4f83f110740cca71c9bcb706
Saurav <sauravsingh1@outlook.in>
2020-10-24 20:19:11 +0530

    dsa

---
+++ reversestring/index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+const reverse = (str) => {
+  var arr = []
+  var ar1 = str.split('')
+  for(var i = ar1.length-1; i >=0; i--){
+    arr.push(ar1[i])
+  }
+  return arr.join('')
+}
+
+//   function reverse(str){
+//   return [...str].reverse().join('')} 
+
+module.exports = reverse;
