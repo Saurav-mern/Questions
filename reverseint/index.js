a0adec85eac862b94ac4f168bfc5bc0d9b227c90
Saurav <sauravsingh1@outlook.in>
2020-10-26 17:10:34 +0530

    parseInt to return number and Math.sign to return the number of same sign

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+
+
+// Solutin 1
+// const reverseInt = (n) => {
+//  return parseInt(n.toString().split('').reverse().join(''))*Math.sign(n)
+// }
+
+function reverseInt(n) {
+
+}
 
 module.exports = reverseInt;
