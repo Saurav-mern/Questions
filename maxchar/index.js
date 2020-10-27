d19a3cf23277e04761a866a39b09079576a96aef
Saurav <sauravsingh1@outlook.in>
2020-10-27 12:53:21 +0530

    for of(string/array) and for in(object)

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var obj ={}
+  var maxValue = 0
+  var maxChar = ''
+  for(let i of str){
+    if(obj.hasOwnProperty(i)){
+      obj[i]++
+    } else {
+      obj[i] = 1
+    }
+  }
+  for(let j in obj){
+    if(obj[j]>maxValue){
+      maxValue = obj[j];
+      maxChar = j
+    }
+  }
+  return maxChar
+}
 
 module.exports = maxChar;
