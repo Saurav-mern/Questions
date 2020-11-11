20f0d577f4e61a6b0b74d1b2f79575adc091ba22
Saurav <sauravsingh1@outlook.in>
2020-11-11 23:25:12 +0530

    another solution

--- fib/index.js
+++ fib/index.js
@@ -18,7 +18,12 @@
 //   console.log(array[n])
 //   console.log(array)
 // }
-
+// const fibonacci = (n) => {
+//   if(n<2){
+//     return n
+//   }
+//   return fibonacci(n-1) + fibonacci(n-2) 
+// }
 function fib(n) {}
 
 module.exports = fib;
