7cda123fabe18298b9d083ae7cfcd45f08447329
Saurav <sauravsingh1@outlook.in>
2020-11-17 19:34:45 +0530

    another way of memoization

--- fib/index.js
+++ fib/index.js
@@ -52,4 +52,20 @@ const fib = memoizer(slowFib)
 //   return fib(n-1) + fib(n-2) 
 // }
 
+//Another Way Of Memoization
+
+function fib (n, prevValues = []) {
+  if(prevValues[n] != null){
+    return prevValue[n]
+  }
+  let result 
+  if (n <= 2){
+    result = 1
+  } else {
+    result = fib(n-1, prevValues) + fib(n-2, prevValues)
+  }
+  prevValues[n] = result
+  return result
+}
+
 module.exports = fib;
