b9916b9a5842ef399bd795d8c1126ae71e2a6f6a
Saurav <sauravsingh1@outlook.in>
2020-11-21 21:53:38 +0530

    linked list and methods

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -15,8 +15,76 @@ class LinkedList {
     }
 
     insertFirst(data){
-        
+        this.head = new Node(data, this.head)
     }
+
+    size(){
+        let counter = 0
+        let node = this.head
+
+        while(node){
+            counter ++
+            node = node.next
+        }
+        return counter
+    }
+
+    getFirst(){
+        return this.head
+    }
+
+    // getLast(){
+    //   let node = this.head
+    //   let last = node
+    //   while(node){
+    //     last = node
+    //     node = node.next
+    //   }
+    //   return last
+    // }
+
+    getLast(){
+        if(!this.head){
+            return null
+        }
+        let node = this.head
+        while(node){
+            if(!node.next){
+                return node
+            }
+            node = node.next
+        }
+    }
+
+    clear(){
+        this.head = null
+    }
+
+    removeFirst(){
+        if(!this.head){
+            return
+        }
+        this.head = this.head.next
+    }
+
+    removeLast(){
+        if(!this.head){
+            return;
+        }
+        if(!this.head.next){
+            this.head = null
+            return
+        }
+        let previous = this.head
+        let node = this.head.next
+        while(node.next){
+            previous = node
+            node = node.next
+        }
+        previous.next = null;
+    }
+
+    insertLast()
 }
 
 module.exports = { Node, LinkedList };
